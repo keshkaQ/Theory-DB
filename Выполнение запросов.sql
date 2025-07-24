0642c58a0c3e382df087a1745e6d575a4a1891fd
keshkaQ <kirill.banar44@gmail.com>
2025-07-24 09:43:30 +0300

    add shop database

---
+++ Выполнение запросов.sql
@@ -0,0 +1,66 @@
+﻿-- 1) Напишите функционал по добавлению пользователя
+INSERT INTO users(login, password, first_name, last_name, patronymic, email, phone)
+VALUES('banar', 'keshka89', 'Кирилл', 'Банарь', 'Юрьевич', 'kirillbanar@gmail.com', '+79503452122');
+-- Проверяем добавился ли пользователь 
+SELECT * FROM users
+WHERE login = 'banar'
+
+-- 2) Напишите функционал по добавлению продукта и присваиванию для него категории
+INSERT INTO products (name,description,price,quantity)
+VALUES('Клавиатура', 'Игровая клавиатура беспроводная','2999.99',20);
+-- Клавиатуре присвоился product_id = 27, у электроники id равен 1, поэтому добавляем в таблицу ProductCategories 
+-- запись со значениями (product_id = 27,category_id = 1)
+INSERT INTO product_categories (product_id, category_id)
+VALUES (27,1);
+
+-- Проверяем категорию у клавиатуры
+SELECT c.name FROM products as p
+JOIN product_categories  as pc on p.product_id = pc.product_id
+JOIN categories as c on c.category_id = pc.category_id
+where p.name = 'Клавиатура'
+
+
+-- 3) Получения продуктов по категории
+SELECT DISTINCT p.name, p.price FROM products as p
+JOIN product_categories as pc ON p.product_id = pc.product_id
+JOIN categories as c on c.category_id = pc.category_id
+where c.name = 'Электроника'
+
+-- 4) Поиск продукта по названию
+SELECT * FROM products as p
+JOIN product_categories as pc ON p.product_id = pc.product_id
+JOIN categories as c on c.category_id = pc.category_id
+WHERE p.name = 'Ноутбук Pro'
+
+-- 5) Получить самые популярные товары TOP (10)
+SELECT TOP 10 p.name, SUM(oi.quantity) as total_ordered
+FROM products p
+JOIN orderItems oi ON p.product_id = oi.product_id
+GROUP BY p.name
+ORDER BY total_ordered DESC;
+
+-- 6) Представление для информации о пользователях (без паролей)
+CREATE VIEW ViewUsers as
+SELECT user_id,
+CASE WHEN patronymic is NULL then first_name + ' ' + last_name
+ELSE first_name + ' ' + last_name + ' ' + patronymic 
+END as full_name, 
+email, phone,registration_date
+FROM Users
+-- Вывод данных из представления
+SELECT * FROM ViewUsers
+
+-- 7) Получить кол-во продуктов (группировка по категориям) купленных для двух категорий 
+-- Группировка по всем категориям и количество купленных товаров
+SELECT c.name, SUM(oi.quantity) as total_purchased
+FROM categories c
+JOIN product_categories pc ON c.category_id = pc.category_id
+JOIN orderItems oi ON pc.product_id = oi.product_id
+GROUP BY c.name
+-- группировка по электронике и бытовой технике
+SELECT c.name, SUM(oi.quantity) as total_purchased
+FROM categories c
+JOIN product_categories pc ON c.category_id = pc.category_id
+JOIN orderItems oi ON pc.product_id = oi.product_id
+WHERE c.name in ('Электроника', 'Бытовая техника')
+GROUP BY c.name
---
+++ Создание  таблиц.sql
@@ -0,0 +1,87 @@
+﻿-- Создание таблицы пользователей
+CREATE TABLE users (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    login NVARCHAR(50) UNIQUE NOT NULL,
+    password NVARCHAR(100) NOT NULL CHECK (LEN(password) BETWEEN 8 AND 100),
+    first_name NVARCHAR(100) NOT NULL,
+    last_name NVARCHAR(100) NOT NULL,
+    patronymic NVARCHAR(100),
+    email NVARCHAR(255) UNIQUE NOT NULL 
+        CHECK (email LIKE '%_@%_.%'
+        AND PATINDEX('%[^a-zA-Z0-9._%@-]%', email) = 0
+        AND email NOT LIKE '@%'  
+        AND email NOT LIKE '%@%@%'   
+        AND email NOT LIKE '%.@%'     
+        AND email NOT LIKE '%..%'  
+        AND email LIKE '%@%.__%'
+        AND LEN(email) >= 5 ), 
+    phone NVARCHAR(20) NOT NULL
+        CHECK ((phone LIKE '+7%' AND LEN(phone) = 12 
+        AND PATINDEX('%[^0-9]%', phone) = 0)
+        OR (phone LIKE '8%' AND LEN(phone) = 11 
+        AND PATINDEX('%[^0-9]%', phone) = 0)),
+    registration_date DATETIME NOT NULL DEFAULT GETDATE(),
+);
+
+-- Создание таблицы категорий
+CREATE TABLE categories (
+    category_id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(200) NOT NULL UNIQUE,
+    description NVARCHAR(1000) NOT NULL,
+);
+
+-- Создание таблицы продуктов
+CREATE TABLE products (
+    product_id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(200) NOT NULL UNIQUE,
+    description NVARCHAR(1000) NOT NULL,
+    price DECIMAL(10,2) NOT NULL CHECK (price > 0),
+    quantity INT NOT NULL DEFAULT 0 CHECK (quantity >= 0),
+    date_added DATETIME NOT NULL DEFAULT GETDATE()
+);
+
+-- Создание таблицы связей товаров и категорий
+CREATE TABLE product_categories (
+    product_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (product_id, category_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
+);
+
+-- Создание таблицы заказов
+CREATE TABLE orders (
+    order_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT NOT NULL,
+    date_created DATETIME NOT NULL DEFAULT GETDATE(),
+    total_price DECIMAL(10,2) NOT NULL CHECK(total_price > 0),
+    order_status NVARCHAR(50) NOT NULL 
+        CHECK(order_status IN ('Ожидает оплаты', 'В обработке', 'Доставлен')),
+    delivery_address NVARCHAR(500) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- Создание таблицы элементов заказа
+CREATE TABLE orderItems (
+    order_item_id INT IDENTITY(1,1) PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    price DECIMAL(10,2) NOT NULL CHECK (price > 0),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+-- Создание таблицы истории заказов
+CREATE TABLE orderHistory (
+    history_id INT IDENTITY(1,1) PRIMARY KEY,
+    order_id INT NOT NULL,
+    previous_status NVARCHAR(50) NULL,
+    new_status NVARCHAR(50) NOT NULL,
+    change_date DATETIME NOT NULL DEFAULT GETDATE(),
+    comment NVARCHAR(300) NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    CHECK (new_status IN ('Ожидает оплаты', 'В обработке', 'Доставлен')),
+    CHECK (previous_status IN ('Ожидает оплаты', 'В обработке', 'Доставлен') OR previous_status IS NULL)
+);
+
